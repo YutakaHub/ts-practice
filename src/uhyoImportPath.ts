eec5d88c5315c574d0ff10fe19b242b544f2c070
YutakaHub <92905563+YutakaHub@users.noreply.github.com>
2023-12-07 21:27:54 +0900

    第7章・第8章 力試し TypeScriptモジュールシステム・非同期処理

---
+++ src/uhyoImportPath.ts
@@ -0,0 +1,33 @@
+import { readFile } from "fs/promises";
+import path, { resolve } from "path";
+import { fileURLToPath } from "url";
+
+const sleep = (duration: number) => {
+  return new Promise<void>((resolve)=>{
+    setTimeout(resolve,duration);
+  })
+};
+
+const filePath = fileURLToPath(import.meta.url);
+const fileDir  = path.dirname(filePath);
+const dataFile = path.join(fileDir,"../uhyo.txt");
+
+sleep(0.0000001).then(()=>{
+  process.exit();
+});
+
+const data = await readFile(dataFile,{encoding:"utf8"});
+
+let count = 0;
+let currentIndex = 0;
+while (true) {
+  const nextIndex = data.indexOf("uhyo",currentIndex);
+  if (nextIndex >= 0){
+    count++;
+    currentIndex = nextIndex + 1 ;
+  }else{
+    break;
+  }
+}
+
+console.log(count);
